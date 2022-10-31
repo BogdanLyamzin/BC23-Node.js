06d7dc4f1e3f4ce2f324d757febb2f93159cca13
unknown <bogdan.lyamzin.d@gmail.com>
2022-10-31 19:20:29 +0200

    finish lesson 5

---
+++ lesson-5/controllers-middlewares-example/controllers/books/getById.js
@@ -0,0 +1,14 @@
+const Book = require("../../models/book")
+
+const {RequestError} = require("../../helpers");
+
+const getById = async(req, res)=> {
+    const {id} = req.params;
+    // const result = await books.getById(id);
+    // if(!result) {
+    //     throw RequestError(404, "Not found")
+    // }
+    // res.json(result)
+}
+
+module.exports = getById;
---
+++ lesson-5/controllers-middlewares-example/controllers/books/removeById.js
@@ -0,0 +1,17 @@
+const Book = require("../../models/book")
+
+const { RequestError } = require("../../helpers");
+
+const removeById = async (req, res) => {
+    const { id } = req.params;
+    // const result = await books.removeById(id);
+    // if (!result) {
+    //     throw RequestError(404)
+    // }
+    // // res.status(204).send()
+    // res.json({
+    //     message: "Delete success"
+    // })
+}
+
+module.exports = removeById
